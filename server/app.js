1bfbf24452772c049255d8cbef4b23765a08d71b
Dalexa Sanjuan <dalexsanjuan@outlook>
2025-08-28 10:28:06 -0500

    Fix cable issues to allow frontend access

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import authRoutes from './src/routes/auth.routes.js';
 import servicesRoutes from './src/routes/services.routes.js';
 import userRoutes from './src/routes/users.routes.js';
 import reservationsRoutes from './src/routes/reservations.routes.js';
+import combosRoutes from "./src/routes/combos.routes.js";
 import { errorMiddleware } from "./src/middlewares/error.js";
 import cookieParser from "cookie-parser";
 import cors from "cors"; 
@@ -12,13 +13,19 @@ const app = express();
 // Core middlewares
 app.use(express.json());       // Parse incoming JSON payloads
 app.use(cookieParser());       // Parse cookies for authentication/session handling
-app.use(cors());               // Enable Cross-Origin Resource Sharing (CORS)
+app.use(cors({
+    origin: 'http://localhost:5173', // URL de tu frontend
+    credentials: true, // Muy importante para cookies
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // API routes
 app.use(authRoutes);           // Authentication & authorization endpoints
 app.use(servicesRoutes);       // Services catalog endpoints
 app.use(userRoutes);           // User management endpoints
 app.use(reservationsRoutes);   // Reservations endpoints
+app.use(combosRoutes);
 
 // Global error handler (must be last)
 app.use(errorMiddleware);
